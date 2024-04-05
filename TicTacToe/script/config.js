6d7179541c4903262ab5f33a130d3d6510e9deaa
sunny <jungsunjin9@gmail.com>
2024-04-05 14:40:19 +0900

    feat: 오버레이 표시, 숨기기

--- TicTacToe/script/config.js
+++ TicTacToe/script/config.js
@@ -2,3 +2,8 @@ function openPlayerConfig() {
     playerConfigOverlayElement.style.display = 'block';
     backdropElement.style.display = 'block';
 }
+
+function closePlayerConfig() {
+    playerConfigOverlayElement.style.display = 'none';
+    backdropElement.style.display = 'none';   
+}
