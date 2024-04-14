9d64f83075241692b4e6aeac2fa15926f393e672
sunny <jungsunjin9@gmail.com>
2024-04-14 22:48:38 +0900

    feat: 사용자 입력 확인, 미입력 에러처리 #1

--- TicTacToe/script/config.js
+++ TicTacToe/script/config.js
@@ -5,12 +5,20 @@ function openPlayerConfig() {
 
 function closePlayerConfig() {
     playerConfigOverlayElement.style.display = 'none';
-    backdropElement.style.display = 'none';   
+    backdropElement.style.display = 'none';  
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent = ''; 
 }
 
 function savePlayerConfig(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
-    const enteredPlayername = formData.get('playername');
-    console.log(enteredPlayername);
+    const enteredPlayername = formData.get('playername').trim(); // '    ' => ''
+
+    if(!enteredPlayername){ //enteredPlayername === ''
+        event.target.firstElementChild.classList.add('error');
+        errorsOutputElement.textContent = 'Please enter a valid name!';
+        return;
+    }
+
 }
