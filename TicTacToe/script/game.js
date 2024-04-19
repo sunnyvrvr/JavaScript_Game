080dee02d96c4ad0b629982bdffd332c20f3c0b8
sunny <jungsunjin9@gmail.com>
2024-04-19 10:39:29 +0900

    fix: 게임플레이어 이름 미제출시 게임시작전 얼럿창 노출 #2

--- TicTacToe/script/game.js
+++ TicTacToe/script/game.js
@@ -1,3 +1,7 @@
 function startNewGame() {
+    if(players[0].name === '' || players[1].name === '') {
+        alert('Please set custom players names for both players!');
+        return;
+    }
     gameAreaElement.style.display = 'block';
 }
