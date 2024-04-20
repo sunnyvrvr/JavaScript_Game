c9112b8645add877ff8af69ce1387b628c4e9860
sunny <jungsunjin9@gmail.com>
2024-04-21 00:25:38 +0900

    feat: 게임플레이어 필드 선택 #2

--- TicTacToe/script/game.js
+++ TicTacToe/script/game.js
@@ -6,6 +6,20 @@ function startNewGame() {
     gameAreaElement.style.display = 'block';
 }
 
+function switchPlayer() {
+    if (activePlayer == 0){
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+}
+
 function selectGameField(event) {
+    if(event.target.tagName !== 'LI'){
+        return;
+    } 
+
     event.target.textContent = players[activePlayer].symbol; //players[0]
+    event.target.classList.add('disabled');
+    switchPlayer();
 }
